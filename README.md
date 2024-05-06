e1a192d136f16d688af5fccfa340e24ed5f6acce
LeoAda <69049724+LeoAda@users.noreply.github.com>
2024-05-06 19:22:43 +0200

    Lot of changes

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # colocatron
 
-https://flask.palletsprojects.com/en/2.3.x/
+https://flask.palletsprojects.com/
 
-https://tinydb.readthedocs.io/en/latest/index.html
+https://flask-login.readthedocs.io/
 
-https://docs.pydantic.dev/latest/concepts/models/
+https://docs.sqlalchemy.org/
 
-https://github.com/LeoAda/colocatron/tree/main
+https://github.com/LeoAda/colocatron/
+
+https://www.webfx.com/tools/emoji-cheat-sheet/
 
 waitress-serve --listen=0.0.0.0:8080 api:app
 ipconfig (windows)
 
 
-https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html
+https://www.geeksforgeeks.org/how-to-add-authentication-to-your-app-with-flask-login/  
+https://flask.palletsprojects.com/en/2.3.x/patterns/flashing/
+https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application
--- src/db.py
+++ src/db.py
@@ -1,18 +1,19 @@
 from typing import Union
-from sqlalchemy import create_engine, ForeignKey, ForeignKey
+from sqlalchemy import create_engine, ForeignKey, ForeignKey, Engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
-
+from flask_login import UserMixin
 
 class Base(DeclarativeBase):
     pass
 
 
-class User(Base):
+class User(Base,UserMixin):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
     username: Mapped[str] = mapped_column()
+    password: Mapped[str] = mapped_column()
     sended_transactions: Mapped[list["Transaction"]] = relationship(
         back_populates="sender", foreign_keys="Transaction.sender_id"
     )
@@ -21,10 +22,12 @@ class User(Base):
     )
     items: Mapped[list["Item"]] = relationship(back_populates="current_user")
     tasks: Mapped[list["Task"]] = relationship(back_populates="current_user")
-
+    
+    def __str__(self) -> str:
+        return self.name
+    
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, username={self.username!r}, items=[{", ".join([str(item.name) for item in self.items])}], tasks=[{", ".join([str(task.name) for task in self.tasks])}])"
-
+        return f"User(id={self.id!r}, name={self.name!r}, username={self.username!r}, items=[{", ".join([str(item.name) for item in self.items])}], tasks=[{", ".join([str(task.name) for task in self.tasks])}], sended_transactions=[{", ".join([str(transaction) for transaction in self.sended_transactions])}], received_transactions=[{", ".join([str(transaction) for transaction in self.received_transactions])}])"
 
 class Transaction(Base):
     __tablename__ = "transactions"
@@ -44,16 +47,27 @@ class Transaction(Base):
     sender_approved: Mapped[bool] = mapped_column()
     receiver_approved: Mapped[bool] = mapped_column()
 
+    def __str__(self) -> str:
+        return f"{self.sender!s} -> {self.receiver!s} - {self.amount} - {self.reason}"
+    
+    def __repr__(self) -> str:
+        return f"Transaction(id={self.id!r}, send={self.sender!s}, receiver_id={self.receiver!s}, reason={self.reason!r}, due_date={self.due_date!r}, amount={self.amount!r}, sender_approved={self.sender_approved!r}, receiver_approved={self.receiver_approved!r})"
+
+
 class Item(Base):
     __tablename__ = "items"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
+    emoji: Mapped[str] = mapped_column(default=":shopping_bags:")
     current_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     current_user: Mapped["User"] = relationship(back_populates="items")
 
+    def __str__(self) -> str:
+        return self.name
+    
     def __repr__(self) -> str:
-        return f"Item(id={self.id!r}, name={self.name!r}, current_user_id={self.current_user_id!r})"
+        return f"Item(id={self.id!r}, name={self.name!r}, emoji={self.emoji!r}, current_user_id={self.current_user_id!r}, current_user={self.current_user!s})"
 
 
 class Task(Base):
@@ -61,63 +75,22 @@ class Task(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
+    emoji: Mapped[str] = mapped_column(default=":broom:")
     current_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     current_user: Mapped["User"] = relationship(back_populates="tasks")
 
+    def __str__(self) -> str:
+        return self.name
+    
     def __repr__(self) -> str:
-        return f"Task(id={self.id!r}, name={self.name!r}, current_user_id={self.current_user_id!r})"
+        return f"Task(id={self.id!r}, name={self.name!r}, emoji={self.emoji!r}, current_user_id={self.current_user_id!r}, current_user={self.current_user!s})"
 
-Actionable = Union[Item, Task]
-
-
-
-
-def assign_to_next_user(session: Session, actionable: Actionable ):
-    users = session.query(User).all()
-    # Get current user
-    current_user = actionable.current_user
-    current_index = users.index(current_user)
-
-    # Get next user
-    next_index = (current_index + 1) % len(users)
-    next_user = users[next_index]
-
-    # Assign item to next user and remove from the current user
-    current_user.items.remove(actionable)
-    actionable.current_user = next_user
-    next_user.items.append(actionable)
-
-    # Commit changes
-    session.commit()
 
 def get_engine():
     db_path = "sqlite:///db.sqlite"
     engine = create_engine(db_path, echo=False)
     return engine
-    
+
+
 def create_tables(engine):
     Base.metadata.create_all(engine)
-
-with Session(engine) as session:
-    new_user1 = User(name="John", username="johnd")
-    session.add(new_user1)
-    new_user2 = User(name="Jane", username="janed")
-    session.add(new_user2)
-    new_item = Item(name="Item1", current_user=new_user1)
-    session.add(new_item)
-    session.commit()
-
-with Session(engine) as session:
-    users = session.query(User).all()
-    print(users)
-
-    items = session.query(Item).all()
-    print(items)
-
-    assign_to_next_user(session, items[0])
-
-    items = session.query(Item).all()
-    print(items)
-
-    users = session.query(User).all()
-    print(users)
